6f13bb92324ab42a867d2ae2819017a6a153612a
washgokk <washgo.kk@gmail.com>
2026-04-22 10:05:42 +0700

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ import { supabase } from '@/lib/supabase'
 import styles from './login.module.css'
 import Logo from '@/components/Branding/Logo'
 
-//owner upload
+//owner upload 2
 /**
  * GlobalLogin Page - v43 Clean Rewrite
  *
